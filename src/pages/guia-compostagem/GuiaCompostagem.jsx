c2017234b106e6af28996529a6e99a37b9d9f9cc
Kawann <kawannmarcelo7@gmail.com>
2025-06-29 14:46:35 -0300

    feat: creating guide two

--- src/pages/guia-compostagem/GuiaCompostagem.jsx
+++ src/pages/guia-compostagem/GuiaCompostagem.jsx
@@ -1,6 +1,9 @@
 import './GuiaCompostagem.css';
 import logo from '../../assets/logo-cor.jpg';
 import card1 from '../../assets/card1.svg';
+import subCard1 from '../../assets/sub-card-1.svg';
+import subCard2 from '../../assets/sub-card-2.svg';
+import subCard3 from '../../assets/sub-card-3.svg';
 
 function GuiaCompostagem() {
   return (
@@ -35,6 +38,26 @@ function GuiaCompostagem() {
             podas de jardim em um material rico em nutrientes, o composto orgânico. 
             Este composto pode ser utilizado como adubo para plantas e jardins.
           </p>
+        </div>
+      </div>
+      <div id="card-guide-two">
+        <h4>2. Tipos de Compostagem Doméstica</h4>
+        <div className="sub-cards">
+          <div id="sub-card-1">
+            <h5>Compostagem em Pilha/Caixote:</h5>
+            <p>Ideal para quem tem um pequeno quintal. Consiste em empilhar os materiais em um local arejado.</p>
+            <img src={subCard1} alt="" />
+          </div>
+          <div id="sub-card-2">
+            <h5>Minhocário (Vermicompostagem):</h5>
+            <p>Utiliza minhocas californianas para acelerar o processo. Ótimo para espaços menores como varandas.</p>
+            <img src={subCard2} alt="" />
+          </div>
+          <div id="sub-card-3">
+            <h5>Compostagem em Baldes/Composteiras Urbanas:</h5>
+            <p>Soluções compactas para apartamentos, utilizando baldes empilháveis ou sistemas selados.</p>
+            <img src={subCard3} alt="" />
+          </div>
         </div>
       </div>
     </div>
