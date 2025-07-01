4ee33d66385a3fb1e795e2e18cfa72f93f6632f3
Kawann <kawannmarcelo7@gmail.com>
2025-07-01 00:16:44 -0300

    feat: creating login function

--- src/pages/autenticacao/Login.jsx
+++ src/pages/autenticacao/Login.jsx
@@ -1,28 +1,55 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom'; // Adicionei Link aqui
 import './Login.css'
+import { toast, ToastContainer } from 'react-toastify';
+import axios from 'axios';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    
-    if (email && password) {
-      navigate('/dashboard'); // Rota para a segunda tela
-    } else {
-      alert('Por favor, preencha todos os campos!');
-    }
+  const url = "http://localhost:4000"
+
+  const validaEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
   };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    
+    if (!email || !password) {
+      toast.error("Todos os campos precisam ser preenchidos!");
+      return;
+    }
+
+    if (!validaEmail(email)) {
+      toast.error("Email inválido!");
+      return;
+    }
+
+    try {
+      await axios.post(`${url}/users/login`, { 
+        email, password 
+      })
+      .then((response) => {
+        const data = response.data;
+        localStorage.setItem("token", data.token)
+      })
+      toast.success("Login feito com sucesso!");
+      navigate("/dashboard");
+    } catch (error) {
+      toast.error("Erro ao tentar fazer Login, tente novamente!");
+    }
+  }
 
   return (
     <div className="login-container">
       <div className="login-card">
         <h2>Composta+</h2>
         
-        <form onSubmit={handleSubmit}>
+        <form>
           <input 
             type="email" 
             placeholder="E-mail" 
@@ -37,13 +64,14 @@ function Login() {
             onChange={(e) => setPassword(e.target.value)}
           />
           
-          <button type="submit" className="login-button">
+          <button onClick={handleLogin} className="login-button">
             Entrar
           </button>
         </form>
         
         <p>Não tem uma conta? <Link to="/cadastro">Cadastre-se</Link></p>
       </div>
+      <ToastContainer />
     </div>
   );
 }
