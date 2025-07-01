b48d66bd59d4f33ae9efb9bea5ac166b44b0ea29
Kawann <kawannmarcelo7@gmail.com>
2025-06-30 21:16:31 -0300

    feat: sending profile type selection to database

--- src/pages/selecao-perfil/SelecaoPerfil.jsx
+++ src/pages/selecao-perfil/SelecaoPerfil.jsx
@@ -1,12 +1,52 @@
+import { toast, ToastContainer } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 import '../autenticacao/Login.css';
+import axios from 'axios';
 
 function SelecaoPerfil() {
   const navigate = useNavigate();
 
-  const handleSelecionarPerfil = (tipo) => {
-    navigate('/dashboard', { state: { tipoPerfil: tipo } });
-  };
+  const url = "http://localhost:4000";
+
+  const setComposter = async () => {
+    const button = document.getElementById("composter-option");
+
+    button.style.backgroundColor = "#114C02";
+
+    const userId = localStorage.getItem("userId")
+    try {
+      await axios.put(`${url}/users/${userId}`, {
+        role: "COMPOSTADOR"
+      }, {
+        headers: {
+          "Authorization": `Bearer ${localStorage.getItem("token")}`
+        }
+      });
+      navigate("/dashboard")
+    } catch (error) {
+      toast.error("Erro ao escolher o tipo de perfil, tente novamente!");
+    }
+  }
+
+  const setDonate = async () => {
+    const button = document.getElementById("donate-option");
+
+    button.style.backgroundColor = "#114C02";
+
+    const userId = localStorage.getItem("userId")
+    try {
+      await axios.put(`${url}/users/${userId}`, {
+        role: "DOADOR"
+      }, {
+        headers: {
+          "Authorization": `Bearer ${localStorage.getItem("token")}`
+        }
+      });
+      navigate("/dashboard")
+    } catch (error) {
+      toast.error("Erro ao escolher o tipo de perfil, tente novamente!");
+    }
+  }
 
   return (
     <div className="login-container">
@@ -16,20 +56,21 @@ function SelecaoPerfil() {
         
         <div className="opcoes-perfil">
           <button 
-            onClick={() => handleSelecionarPerfil('Compostador(a)')}
-            className="perfil-option"
+            onClick={setComposter}
+            id="composter-option"
           >
             Sou Compostador(a)
           </button>
           
           <button 
-            onClick={() => handleSelecionarPerfil('Doador(a)')}
-            className="perfil-option"
+            onClick={setDonate}
+            id="donate-option"
           >
             Sou Doador(a)
           </button>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 }
